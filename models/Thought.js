dec4968a253c16cab0b6ea7dfd7ef5376c79a498
Gabriel Guerin <gjguerin96@gmail.com>
2022-01-24 23:58:52 -0800

    added to models, added config file

---
+++ models/Thought.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    username: {
+        type: String,
+        required: true
+    }
+  }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, 'Please enter a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+  }
+);
+
+userSchema
+  .virtual('friendCount')
+  
+  .get(function () {
+    return friends.length;
+  })
+
+const User = model('user', userSchema);
+
+module.exports = User;
